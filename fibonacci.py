d0061f2fb0bd98bec0c8782d7696df5bbad95024
sisaghol <sisaghol@cisco.com>
2019-08-31 22:56:38 -0700

    optimized

--- fibonacci.py
+++ fibonacci.py
@@ -1,3 +1,10 @@
+'''
+$ python fibonacci.py 300000
+
+0:00:01.907796 Fibo03 (Dynamic O(n) array) seconds
+0:00:00.800295 Fibo04 (Dynamic O(1) array) seconds
+0:00:00.781308 Fibo05 (Dynamic O(1) array) seconds
+'''
 import sys
 import datetime
 from functools import lru_cache
@@ -46,7 +53,7 @@ def fibo03(n):
 def fibo04(n):
     '''
     dynamic programming, with O(1) size of array
-    most efficient and faster for big inputs
+    more efficient and faster for big inputs
     '''
     answers = [1, 1]
     for i in range(2, n):
@@ -55,20 +62,41 @@ def fibo04(n):
 
     return answers[-1]
 
+def fibo05(n):
+    '''
+    dynamic programming, with O(1) size of array
+    most efficient and faster
+    '''
+    if n < 2:
+        return 1
+
+    answers = [1,1, None]
+    for i in range(2, n):
+        answers[2] = answers[1] + answers[0]
+        answers[0], answers[1] = answers[1], answers[2]
+
+    return answers[-1]
+
 if __name__ == "__main__":
     n = int(sys.argv[1])
-    # for i in range(1, n + 1):
     t1 = datetime.datetime.now()
-    # a = fibo_big(i, 950)
-    a = fibo04(n)
+    a = fibo03(n)
     td1 = datetime.datetime.now() - t1
+    #
     t2 = datetime.datetime.now()
-    b = fibo03(n)
+    b = fibo04(n)
     td2 = datetime.datetime.now() - t2
-    if a == b:
-        print(a)
+    #
+    t3 = datetime.datetime.now()
+    c = fibo05(n)
+    td3 = datetime.datetime.now() - t3
+    #
+    if a == b and b == c:
+        pass
+        # print(a)
     else:
         print('Error')
-    print(td1, 'Fibo04 seconds')
-    # print(b)
-    print(td2, 'Fibo03 seconds')
+
+    print(td1, 'Fibo03 (Dynamic O(n) array) seconds')
+    print(td2, 'Fibo04 (Dynamic O(1) array) seconds')
+    print(td3, 'Fibo05 (Dynamic O(1) array) seconds')
